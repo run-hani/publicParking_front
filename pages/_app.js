ad59ca33dce3364f5212818be3e656b7f911140d
run-hani <hanirun@kakao.com>
2022-04-19 01:30:50 +0900

    delete: 없는 리듀서 삭제

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,7 @@ const App = ({ Component, pageProps }) => {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width, user-scalable=no, 
-      initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
         ></meta>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Public Parking front</title>
